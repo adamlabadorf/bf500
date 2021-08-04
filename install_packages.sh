7ee283130c94cb2a070f0aa0cb031417001929a8
Adam Labadorf <labadorf@bu.edu>
2021-08-04 12:24:41 -0400

    added python packages for project test env

--- install_packages.sh
+++ install_packages.sh
@@ -1,7 +1,7 @@
 #conda create -n bf550 python=3.9
 #conda activate bf550
 conda install -c conda-forge ruby=2.7.2 gxx_linux-64
-pip install pandas
+pip install pandas snakemake ncbi-acc-download
 
 gem install bundler
 
